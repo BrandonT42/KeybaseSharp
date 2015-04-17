88da4180724c4bab2220a66b8d0977ec968c96c5
KenBonny <bonny.ken@gmail.com>
2015-04-17 14:06:46 +0200

    Renamed property Usernames to Matches to be more in line with the api.

--- KeybaseSharp/Model/User/Discover/DiscoverUsernames.cs
+++ KeybaseSharp/Model/User/Discover/DiscoverUsernames.cs
@@ -1,11 +1,9 @@
 ﻿using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace KenBonny.KeybaseSharp.Model.User.Discover
 {
     public class DiscoverUsernames : BaseObject
     {
-        [JsonProperty(PropertyName = "matches")]
-        public List<string> Usernames { get; set; }
+        public List<string> Matches { get; set; }
     }
 }

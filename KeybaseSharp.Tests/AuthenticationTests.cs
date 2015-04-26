dd3fb4508f02688ae2ae9b620054cc7034c0b1b4
Ken Bonny <bonny.ken@gmail.com>
2015-04-26 18:20:36 +0200

    Added tests for Authentication.

---
+++ KeybaseSharp.Tests/AuthenticationTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KenBonny.KeybaseSharp.Tests
+{
+    [TestClass]
+    public class AuthenticationTests
+    {
+        private const string KnownUser = "kenbonny";
+        private const string KnownUserFullName = "Ken Bonny";
+        private const string UnknownUser = "bla";
+        
+        [TestMethod]
+        public void Authenticate_Known_User()
+        {
+            var loginTask = KeybaseApi.Authenticate(KnownUser, "Fill in when necessary");
+            Assert.IsNotNull(loginTask);
+
+            var login = loginTask.Result;
+            Assert.IsNotNull(login.User);
+            Assert.AreEqual(0, login.Status.Code);
+            Assert.AreEqual(KnownUserFullName, login.User.Profile.FullName);
+        }
+        
+        [TestMethod]
+        public void Authenticate_Known_User_Bad_Password()
+        {
+            var loginTask = KeybaseApi.Authenticate(KnownUser, "NotMyPassword");
+            Assert.IsNotNull(loginTask);
+
+            var login = loginTask.Result;
+            Assert.IsNull(login.User);
+            Assert.IsTrue(login.Status.Code != 0);
+        }
+
+        [TestMethod]
+        public void Authenticate_Unknown_User()
+        {
+            var loginTask = KeybaseApi.Authenticate(UnknownUser, "DoesntMatter");
+            Assert.IsNotNull(loginTask);
+
+            try
+            {
+                var login = loginTask.Result;
+                Assert.IsTrue(login.Status.Code != 0);
+            }
+            catch (Exception exception)
+            {
+                Assert.IsNotNull(exception);
+            }
+        }
+    }
+}
---
+++ KeybaseSharp/Authentication.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using KenBonny.KeybaseSharp.Model.Authentication;
+
+namespace KenBonny.KeybaseSharp
+{
+    internal static class Authentication
+    {
+        /// <summary>
+        /// The user presents a username or email address to the server, and the server responds with the salt.
+        /// </summary>
+        /// <param name="username">Username or email address.</param>
+        /// <returns>A salt.</returns>
+        internal static Task<Salt> GetSaltAsync(string username)
+        {
+            var address = string.Format("_/api/1.0/getsalt.json?email_or_username={0}", username);
+
+            return KeybaseApi.Get<Salt>(address);
+        }
+
+        internal static Task<Login> LoginAsync(string username, Password password, string session)
+        {
+            var address = string.Format("_/api/1.0/login.json?email_or_username={0}&hmac_pwh={1}&login_session={2}",
+                username, password, session);
+
+            var parameters = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("email_or_username", username),
+                new KeyValuePair<string, string>("hmac_pwh", password.ToString()),
+                new KeyValuePair<string, string>("login_session", session)
+            };
+
+            return KeybaseApi.Post<Login>(address, parameters);
+        } 
+    }
+}
